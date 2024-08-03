70409a2f62323da4197a5ac71614b00296f330c3
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-08-03 17:35:11 -0400

    Initial Commit

---
+++ Catan in AR/Catan_in_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catan_in_ARApp.swift
+//  Catan in AR
+//
+//  Created by Parth Antala on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Catan_in_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
